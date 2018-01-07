dcf575d7d44ea855322d4d05cfc8aae8d88a785a
rroyGit <rupam95@yahoo.com>
2018-01-06 23:13:21 -0500

    -Added 2 buttons on calculator fragment L button copies the result and pastes it on the left editText R button copies the result and pastes it on the right editText -Code refactor

--- app/src/main/java/com/example/roy/navapp/CalculatorFragment.java
+++ app/src/main/java/com/example/roy/navapp/CalculatorFragment.java
@@ -11,7 +11,6 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
@@ -20,19 +19,19 @@ import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.text.NumberFormat;
+import java.util.StringTokenizer;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CalculatorFragment extends Fragment {
+public class CalculatorFragment extends Fragment implements View.OnClickListener {
 
     TextView result;
     EditText num1, num2;
-    Button aB, mB, mulB,divB, cB;
+    Button addB, subB, mulB, divB, clearB, leftCopyB, rightCopyB;
     ScrollView scrollView;
     final int space_btw_editText = 15;
     final int editText1_width = 500, editText2_width = 500;
@@ -52,154 +51,237 @@ public class CalculatorFragment extends Fragment {
         result.setTextIsSelectable(true);
         num1 = (EditText) view.findViewById(R.id.num1);
         num2 = (EditText) view.findViewById(R.id.num2);
-        aB = (Button) view.findViewById(R.id.addB);
-        mB = (Button) view.findViewById(R.id.minusB);
-        cB = (Button) view.findViewById(R.id.clear);
+        addB = (Button) view.findViewById(R.id.addB);
+        subB = (Button) view.findViewById(R.id.minusB);
+        clearB = (Button) view.findViewById(R.id.clear);
         mulB = (Button) view.findViewById(R.id.mulB);
         divB = (Button) view.findViewById(R.id.divB);
+        leftCopyB =(Button) view.findViewById(R.id.leftCopy);
+        rightCopyB =(Button) view.findViewById(R.id.rightCopy);
         scrollView = (ScrollView) view.findViewById(R.id.scrollViewCalculator);
 
-        positonEditText();
+        positionEditText();
+        controlScroll();
+        clearB.setOnClickListener(this);
+        mulB.setOnClickListener(this);
+        divB.setOnClickListener(this);
+        addB.setOnClickListener(this);
+        subB.setOnClickListener(this);
+        leftCopyB.setOnClickListener(this);
+        rightCopyB.setOnClickListener(this);
+    }
 
+    public static void hideKeyboard(Activity thisActivity, View view){
+        if(view != null) {
+            InputMethodManager imm;
+            imm = (InputMethodManager) thisActivity.getSystemService(Context.INPUT_METHOD_SERVICE);
+            assert imm != null;
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
-        cB.setOnClickListener(new View.OnClickListener() {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calculator, container, false);
+    }
 
-            @Override
-            public void onClick(View v) {
-                result.setText("");
-                num1.getText().clear();
-                num2.getText().clear();
+    private void positionEditText(){
+
+        //position the EditTexts correctly
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        int totalTextWidth =  editText1_width+editText2_width+space_btw_editText;
+        int device_width = displayMetrics.widthPixels;
+        int startIndex= (device_width-totalTextWidth)/2;
+
+        RelativeLayout.LayoutParams editT1 = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        RelativeLayout.LayoutParams editT2 = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        editT1.setMargins(startIndex,400,0,0);
+        editT2.setMargins(startIndex+editText1_width+space_btw_editText,400, 0, 0);
+        num1.setLayoutParams(editT1);
+        num1.setWidth(editText1_width);
+        num2.setLayoutParams(editT2);
+        num2.setWidth(editText2_width);
+    }
+
+    private void multiplyNumbers(){
+        if (num1.getText().toString().trim().isEmpty() ||
+                num2.getText().toString().trim().isEmpty()) {
+            result.setText(getString(R.string.error));
+            hideKeyboard(getActivity(), getView());
+            return;
+        }
+
+        double a = Double.parseDouble(num1.getText().toString());
+        double b = Double.parseDouble(num2.getText().toString());
+        if (a > Integer.MAX_VALUE || b > Integer.MAX_VALUE) {
+            result.setText(R.string.error3);
+            hideKeyboard(getActivity(), getView());
+            return;
+        }
+
+        n1 = Double.parseDouble(num1.getText().toString());
+        n2 = Double.parseDouble(num2.getText().toString());
+        double res = n1 * n2;
+
+        if (res < 0 || res > Integer.MAX_VALUE) {
+            result.setText(R.string.error3);
+            hideKeyboard(getActivity(), getView());
+            return;
+        }
+
+        res_num = n1 * n2;
+        result.setText(NumberFormat.getInstance().format(res_num));
+    }
+
+    private void divideNumbers(){
+        if (num1.getText().toString().trim().isEmpty() ||
+                num2.getText().toString().trim().isEmpty()) {
+            result.setText(getString(R.string.error));
+            hideKeyboard(getActivity(), getView());
+            return;
+        }
+
+        double a = Double.parseDouble(num1.getText().toString());
+        double b = Double.parseDouble(num2.getText().toString());
+        if (a > Integer.MAX_VALUE || b > Integer.MAX_VALUE) {
+            result.setText(R.string.error3);
+            hideKeyboard(getActivity(), getView());
+            return;
+
+        }
+
+        n1 = Double.parseDouble(num1.getText().toString());
+        n2 = Double.parseDouble(num2.getText().toString());
+        if (n2 == 0) {
+            result.setText(getString(R.string.error2));
+            hideKeyboard(getActivity(), getView());
+            return;
+        }
+        res_num = n1 / n2;
+        result.setText(NumberFormat.getInstance().format(res_num));
+    }
+
+    private void addNumbers(){
+        if (num1.getText().toString().trim().isEmpty() ||
+                num2.getText().toString().trim().isEmpty()) {
+            result.setText(getString(R.string.error));
+            hideKeyboard(getActivity(), getView());
+            return;
+        }
+        double a = Double.parseDouble(num1.getText().toString());
+        double b = Double.parseDouble(num2.getText().toString());
+        if (a > Integer.MAX_VALUE || b > Integer.MAX_VALUE) {
+            result.setText(R.string.error3);
+            hideKeyboard(getActivity(), getView());
+            return;
+        }
+        n1 = Double.parseDouble(num1.getText().toString());
+        n2 = Double.parseDouble(num2.getText().toString());
+        if (n1 > Double.MAX_VALUE - n2) {
+            result.setText(getString(R.string.error3));
+            hideKeyboard(getActivity(), getView());
+            return;
+        }
+        res_num = n1 + n2;
+        result.setText(NumberFormat.getInstance().format(res_num));
+    }
+
+    private void subtractNumbers(){
+        if (num1.getText().toString().trim().isEmpty() ||
+                num2.getText().toString().trim().isEmpty()) {
+            result.setText(getString(R.string.error));
+            hideKeyboard(getActivity(), getView());
+            return;
+        }
+        double a = Double.parseDouble(num1.getText().toString());
+        double b = Double.parseDouble(num2.getText().toString());
+        if (a > Integer.MAX_VALUE || b > Integer.MAX_VALUE) {
+            result.setText(R.string.error3);
+            hideKeyboard(getActivity(), getView());
+            return;
+        }
+        if (n1 > Double.MAX_VALUE - n2) {
+            result.setText(getString(R.string.error3));
+            hideKeyboard(getActivity(), getView());
+            return;
+        }
+        n1 = Double.parseDouble(num1.getText().toString());
+        n2 = Double.parseDouble(num2.getText().toString());
+        res_num = n1 - n2;
+        result.setText(NumberFormat.getInstance().format(res_num));
+    }
+
+    private void clearNumbers(){
+        result.setText("");
+        num1.getText().clear();
+        num2.getText().clear();
+    }
+
+    private void leftCopyPaste(){
+        String retString = result.getText().toString();
+        if(retString.compareTo("Too large") !=0 & retString.compareTo("Error") != 0 & retString.compareTo("Undefined") !=0 & retString.compareTo("") != 0) {
+            StringTokenizer stringTokenizer = new StringTokenizer(result.getText().toString(), ",");
+            if(stringTokenizer.countTokens() == 1){
+                num1.setText(retString);
+            }else {
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i <= stringTokenizer.countTokens() + 1; i++) {
+                    stringBuilder.append(stringTokenizer.nextToken());
+                }
+                num1.setText(stringBuilder.toString());
             }
-        });
-
-        mulB.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                if (num1.getText().toString().trim().isEmpty() ||
-                        num2.getText().toString().trim().isEmpty()) {
-                    result.setText(getString(R.string.error));
-                    hideKeyboard(getActivity(), getView());
-                    return;
+        }
+    }
+    private void rightCopyPaste(){
+        String retString = result.getText().toString();
+        if(retString.compareTo("Too large") !=0 & retString.compareTo("Error") != 0 & retString.compareTo("Undefined") !=0 & retString.compareTo("") != 0) {
+            StringTokenizer stringTokenizer = new StringTokenizer(result.getText().toString(), ",");
+            if(stringTokenizer.countTokens() == 1){
+                num1.setText(retString);
+            }else {
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i <= stringTokenizer.countTokens() + 1; i++) {
+                    stringBuilder.append(stringTokenizer.nextToken());
                 }
-
-                double a = Double.parseDouble(num1.getText().toString());
-                double b = Double.parseDouble(num2.getText().toString());
-                if (a > Integer.MAX_VALUE || b > Integer.MAX_VALUE) {
-                    result.setText(R.string.error3);
-                    hideKeyboard(getActivity(), getView());
-                    return;
-                }
-
-                n1 = Double.parseDouble(num1.getText().toString());
-                n2 = Double.parseDouble(num2.getText().toString());
-                double res = n1 * n2;
-
-                if (res < 0 || res > Integer.MAX_VALUE) {
-                    result.setText(R.string.error3);
-                    hideKeyboard(getActivity(), getView());
-                    return;
-                }
-
-                res_num = n1 * n2;
-                result.setText(NumberFormat.getInstance().format(res_num));
-                hideKeyboard(getActivity(), getView());
+                num2.setText(stringBuilder.toString());
             }
-        });
+        }
+    }
 
-        divB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (num1.getText().toString().trim().isEmpty() ||
-                        num2.getText().toString().trim().isEmpty()) {
-                    result.setText(getString(R.string.error));
-                    hideKeyboard(getActivity(), getView());
-                    return;
-                }
+    @Override
+    public void onClick(View view){
+        switch(view.getId()){
+            case R.id.clear:
+                clearNumbers();
+                break;
+            case R.id.mulB:
+                multiplyNumbers();
+                break;
+            case R.id.divB:
+                divideNumbers();
+                break;
+            case R.id.addB:
+                addNumbers();
+                break;
+            case R.id.minusB:
+                subtractNumbers();
+                break;
+            case R.id.leftCopy:
+                leftCopyPaste();
+                break;
+            case R.id.rightCopy:
+                rightCopyPaste();
+                break;
+            default:
+        }
+        hideKeyboard(getActivity(), getView());
+    }
 
-                double a = Double.parseDouble(num1.getText().toString());
-                double b = Double.parseDouble(num2.getText().toString());
-                if (a > Integer.MAX_VALUE || b > Integer.MAX_VALUE) {
-                    result.setText(R.string.error3);
-                    hideKeyboard(getActivity(), getView());
-                    return;
-
-                }
-
-                n1 = Double.parseDouble(num1.getText().toString());
-                n2 = Double.parseDouble(num2.getText().toString());
-                if (n2 == 0) {
-                    result.setText(getString(R.string.error2));
-                    hideKeyboard(getActivity(), getView());
-                    return;
-                }
-                res_num = n1 / n2;
-                result.setText(NumberFormat.getInstance().format(res_num));
-                hideKeyboard(getActivity(), getView());
-            }
-        });
-
-        aB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (num1.getText().toString().trim().isEmpty() ||
-                        num2.getText().toString().trim().isEmpty()) {
-                    result.setText(getString(R.string.error));
-                    hideKeyboard(getActivity(), getView());
-                    return;
-                }
-                double a = Double.parseDouble(num1.getText().toString());
-                double b = Double.parseDouble(num2.getText().toString());
-                if (a > Integer.MAX_VALUE || b > Integer.MAX_VALUE) {
-                    result.setText(R.string.error3);
-                    hideKeyboard(getActivity(), getView());
-                    return;
-                }
-                n1 = Double.parseDouble(num1.getText().toString());
-                n2 = Double.parseDouble(num2.getText().toString());
-                if (n1 > Double.MAX_VALUE - n2) {
-                    result.setText(getString(R.string.error3));
-                    hideKeyboard(getActivity(), getView());
-                    return;
-                }
-                res_num = n1 + n2;
-                result.setText(NumberFormat.getInstance().format(res_num));
-
-                hideKeyboard(getActivity(), getView());
-
-            }
-
-        });
-
-        mB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (num1.getText().toString().trim().isEmpty() ||
-                        num2.getText().toString().trim().isEmpty()) {
-                    result.setText(getString(R.string.error));
-                    hideKeyboard(getActivity(), getView());
-                    return;
-                }
-                double a = Double.parseDouble(num1.getText().toString());
-                double b = Double.parseDouble(num2.getText().toString());
-                if (a > Integer.MAX_VALUE || b > Integer.MAX_VALUE) {
-                    result.setText(R.string.error3);
-                    hideKeyboard(getActivity(), getView());
-                    return;
-                }
-                if (n1 > Double.MAX_VALUE - n2) {
-                    result.setText(getString(R.string.error3));
-                    hideKeyboard(getActivity(), getView());
-                    return;
-                }
-                n1 = Double.parseDouble(num1.getText().toString());
-                n2 = Double.parseDouble(num2.getText().toString());
-                res_num = n1 - n2;
-                result.setText(NumberFormat.getInstance().format(res_num));
-                hideKeyboard(getActivity(), getView());
-            }
-        });
-
+    private void controlScroll(){
         num1.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -249,54 +331,5 @@ public class CalculatorFragment extends Fragment {
 
             }
         });
-
     }
-
-    public static void hideKeyboard(Activity thisActivity, View view){
-        if(view != null) {
-            InputMethodManager imm;
-            imm = (InputMethodManager) thisActivity.getSystemService(Context.INPUT_METHOD_SERVICE);
-            assert imm != null;
-            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-        }
-    }
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calculator, container, false);
-    }
-
-    private void positonEditText(){
-        Thread editTextThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                //position the EditTexts correctly
-                DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
-                int totalTextWidth =  editText1_width+editText2_width+space_btw_editText;
-                int device_width = displayMetrics.widthPixels;
-                int startIndex= (device_width-totalTextWidth)/2;
-
-                RelativeLayout.LayoutParams editT1 = new RelativeLayout.LayoutParams(
-                        RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-                RelativeLayout.LayoutParams editT2 = new RelativeLayout.LayoutParams(
-                        RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-                editT1.setMargins(startIndex,400,0,0);
-                editT2.setMargins(startIndex+editText1_width+space_btw_editText,400, 0, 0);
-                num1.setLayoutParams(editT1);
-                num1.setWidth(editText1_width);
-                num2.setLayoutParams(editT2);
-                num2.setWidth(editText2_width);
-            }
-        });
-
-        editTextThread.start();
-        try{
-            editTextThread.join();
-        }catch (Exception e){
-            Toast.makeText(getContext(), "Error: "+e, Toast.LENGTH_LONG).show();
-        }
-    }
-
 }
